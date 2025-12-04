583e4f62232b024855ce178322837d30d460a24d
Puskar Thapa Magar <puskarmagar0215@gmail.com>
2025-12-04 23:24:55 +0545

    Learning router react

--- react-router/src/App.jsx
+++ react-router/src/App.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Contact from './pages/Contact'
 
 const App = () => {
   return (
     <div>
-      App
+      <div className='nav bg-black text-white flex justify-between items-center'>
+        <div className='p-5 text-2xl font-bold'>
+          <a href="/">Kanung</a>
+        </div>
+        <div className='flex gap-10 p-5'>
+          <a href="/">Home</a>
+          <a href="/about">About</a>
+          <a href="/contact">Contact</a>
+        </div>
+      </div>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+      </Routes>
     </div>
   )
 }
